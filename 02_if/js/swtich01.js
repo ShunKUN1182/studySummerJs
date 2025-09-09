4fae9c0da4fd3e643f53c738c7f45c94728a827d
ShunKUN1182 <2250378@ecc.ac.jp>
2025-09-10 00:54:25 +0900

    if文終わり

---
+++ 02_if/js/swtich01.js
@@ -0,0 +1,22 @@
+let code = "HTML";
+
+// code = "CSS";
+
+// code = "JavaScript";
+
+// code = "syunyaScript";
+
+switch (code) {
+    case "HTML":
+        console.log("拡張子は.htmlです");
+        break;
+    case "CSS":
+        console.log("拡張子は.cssです");
+        break;
+    case "JavaScript":
+        console.log("拡張子は.jsです");
+        break;
+    default:
+        console.log("わからないファイルです");
+        break;
+}
