57fbeffa94ee9a73a8d84ac4a5f5d31510cb4796
ShunKUN1182 <2250378@ecc.ac.jp>
2025-09-10 16:59:14 +0900

    途中

--- 03_for/js/for06_practice.js
+++ 03_for/js/for06_practice.js
@@ -2,3 +2,21 @@
 let wallet = 12800;
 //価格
 let price = 980;
+
+// for(let i = 0; i < 1;){
+//     if (wallet >= 980) {
+//         wallet - 980
+//         console.log("鶏肉購入");
+//     }else{
+//         i + 1
+//     }
+// };
+
+for(let i = 0; wallet < price; i++){
+    if (wallet >= price) {
+        wallet -= price
+    }else{
+        console.log(`${i + 1}回購入することができました`);
+        console.log(`残りの金額は${wallet}円です`);
+    }
+};
