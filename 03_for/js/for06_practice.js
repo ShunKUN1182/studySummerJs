66b3bef3b998a37edac79055e43db626273c53b6
ShunKUN1182 <2250378@ecc.ac.jp>
2025-09-12 00:19:13 +0900

    for文終了

--- 03_for/js/for06_practice.js
+++ 03_for/js/for06_practice.js
@@ -4,11 +4,25 @@ let wallet = 12800;
 let price = 980;
 
 
-for(let i = 0; wallet > price; i++){
+
+for(let i = 0; i < 20; i++){
+    
     if (wallet >= price) {
         wallet -= price
+        console.log("購入しました");
     }else{
-        console.log(`${i + 1}回購入することができました`);
-        console.log(`残りの金額は${wallet}円です`);
+        console.log("購入できませんでした");
+        console.log(`${i}回購入できました。残り金額は${wallet}円です。`);
+        i = 20;
     }
 };
+    
+    // for(let i = 0; wallet < price; i++){
+    //     if (wallet >= price) {
+    //         wallet = wallet - price
+    //         console.log(`${i+1}回目の購入`);
+    //         console.log(`残り金額${wallet}円`);
+    //     }
+    //     console.log(`${i + 1}回購入することができました`);
+    //     console.log(`残りの金額は${wallet}円です`);
+    // };
