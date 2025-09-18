9933fff13d2c78292576227d608198c272345a10
ShunKUN1182 <2250378@ecc.ac.jp>
2025-09-18 16:55:26 +0900

    loop終了

--- 07_loop/js/loop06_practice.js
+++ 07_loop/js/loop06_practice.js
@@ -1,3 +1,38 @@
-const fruits = document.querySelectorAll(".price");
+const result = document.getElementById("total");
+const addName = document.getElementById("addName");
+const addPrice = document.getElementById("addPrice");
+const btn = document.getElementById("btn");
+const addTable = document.querySelector("tbody");
+let fruitBoxs = [];
 
-console.log(fruits);
+console.log(addTable);
+
+
+function calculation() {
+
+    const fruitsPrice = document.querySelectorAll(".price");
+    
+    fruitsPrice.forEach((e , i) => {
+        fruitBoxs[i] = parseInt(e.textContent);
+        console.log(fruitBoxs);
+    });
+    
+    const totalPrice = fruitBoxs.reduce((a,b) => a + b);
+    
+    console.log(totalPrice);
+    
+    result.textContent = "";
+    result.insertAdjacentText("beforeend" , totalPrice + "円");
+    
+}    
+
+calculation();
+
+btn.addEventListener("click" , () =>{
+    addTable.insertAdjacentHTML("beforeend" , `<tr><td class="class">${addName.value}</td><td class="price">${addPrice.value}</td></tr>`)
+    calculation();
+})
+
+
+
+
---
+++ 08_timer/js/timer01.js
@@ -0,0 +1,5 @@
+const text = document.getElementById("text");
+
+setTimeout(()=>{
+    text.innerText = "二秒後に表示される"
+},2000);
