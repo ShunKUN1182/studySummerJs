f676dba3d8de0125687218ddc5df4acb03aeca46
ShunKUN1182 <2250378@ecc.ac.jp>
2025-09-30 09:34:37 +0900

    timer02終了

--- 08_timer/js/timer02.js
+++ 08_timer/js/timer02.js
@@ -1,8 +1,18 @@
 console.log("foo");
-const counter = document.getElementById
+
+const counter = document.getElementById("counter");
+let count = 0;
+const timer = 2000;
 
 function setTimer() {
-    if (count < 10) {
-        
+    if (count < 30) {
+        count++;
+        console.log(count);
+        counter.innerHTML = `${count}回`;
+        setTimeout(setTimer , timer)
+    }else{
+        return false
     }
 }
+
+setTimer();
