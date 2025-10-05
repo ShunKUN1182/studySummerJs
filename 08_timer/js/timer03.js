32a4d0b56464d4c8ae018246125d19a2dea6476b
ShunKUN1182 <2250378@ecc.ac.jp>
2025-10-05 22:44:18 +0900

    timer03終了

--- 08_timer/js/timer03.js
+++ 08_timer/js/timer03.js
@@ -1,1 +1,16 @@
 const arr = ["HTML","CSS","JavaScript","Pug","Sass","TypeScript"];
+
+const list = document.getElementById("skill");
+let count = 0;
+const addTime = 3000;
+
+setInterval(()=>{
+
+    if (count < arr.length) {
+        // 確認用ログ console.log("foo");
+        list.innerHTML += `<li>${arr[count]}</li>`
+        count++;
+    }else{
+        return false;
+    }
+},addTime);
