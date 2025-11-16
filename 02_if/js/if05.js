01216227a3c5930c7f2268c0a0f3c6408f27cc1b
ShunKUN1182 <2250378@ecc.ac.jp>
2025-11-17 00:45:00 +0900

    もう一度過去の難問にチャレンジ！！！

--- 02_if/js/if05.js
+++ 02_if/js/if05.js
@@ -1,17 +1,28 @@
-const photoshopScore = Number(prompt("Photoshopの点数は？"));
-const illustratorScore = Number(prompt("Illustratorの点数は？"));
-
-const totalScore = photoshopScore + illustratorScore;
+const photoShopScore = Number(prompt("photoshop何点なの？"));
+const IllustratorScore = Number(prompt("Illustrator何点なの？"));
+const totalScore = photoShopScore + IllustratorScore;
 const averageScore = totalScore / 2;
-console.log(averageScore , totalScore);
 
+if (photoShopScore >= 0 && photoShopScore <= 100) {
+    if (IllustratorScore >= 0 && IllustratorScore <= 100) {
+        averageScoreCheck();
+    } else {
+        errorMessage();
+    }
+} else {
+    errorMessage();
+}
 
-if (averageScore >= 0 && averageScore <= 30) {
-    console.log("がんばりなさい");
-}else if (averageScore > 30 && averageScore <=60) {
-    console.log("まずまずね");
-}else if (averageScore > 60 && averageScore <= 100) {
-    console.log("頑張ったね");
-}else{
-    console.log("嘘つくんじゃない");
-};
+function errorMessage() {
+    alert("1~100の数値を入力して下さい");
+}
+
+function averageScoreCheck() {
+    if (averageScore >= 61 && averageScore <= 100) {
+        console.log("頑張ったね");
+    } else if (averageScore >= 31 && averageScore <= 60) {
+        console.log("まずまずね");
+    } else if (averageScore >= 0 && averageScore <= 30) {
+        console.log("がんばりなさい");
+    }
+}
