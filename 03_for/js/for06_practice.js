71669ae894fc9b06949ed590209d393216e7bc67
ShunKUN1182 <2250378@ecc.ac.jp>
2025-11-27 13:26:36 +0900

    確認

--- 03_for/js/for06_practice.js
+++ 03_for/js/for06_practice.js
@@ -5,28 +5,10 @@ let price = 980;
 
 let sellPrice = 750;
 
-let buyCount = 0;
 
-for (let j = 1; j <= 20; j++) {
-    if (j < 4) {
-        wallet -= price;
-        buyCount++;
-        console.log(wallet, buyCount);
-    } else if (j == 5) {
-        price = sellPrice;
-        // console.log(wallet);
-    } else if (price <= wallet) {
-        wallet -= price;
-        buyCount++;
-        console.log(wallet, buyCount);
-    } else {
-        console.log(`買い物終了,${buyCount}回購入しました。残り${wallet}円です`);
-        break;
-    }
-}
 
 // for(let i = 0; i < 20; i++){
-
+    
 //     if (wallet >= price) {
 //         wallet -= price
 //         console.log("購入しました");
@@ -37,36 +19,38 @@ for (let j = 1; j <= 20; j++) {
 //     }
 // };
 
-// for (let i = 0; i < 20; i++) {
-//     if (wallet >= price) {
-//         if (i < 5) {
-//             wallet -= price;
-//             // console.log("通常価格で購入");
-//         } else {
-//             wallet -= sellPrice;
-//             // console.log("割引価格で購入");
-//         }
-//     } else if (wallet >= sellPrice) {
-//         wallet -= sellPrice;
-//         // console.log("割引価格で購入");
-//     } else {
-//         console.log("残高不足です");
-//         // console.log(`${i}回購入できました、残り金額は${wallet}円です。`);
-//         break;
-//     }
 
-//     console.log(`${i + 1}回目の買い物終了、残金は${wallet}円です`);
-// }
+for(let i = 0; i < 20; i++){
+    if (wallet >= price) {
+        if (i < 5) {
+            wallet -= price;
+            // console.log("通常価格で購入");            
+        }else{
+            wallet -= sellPrice;
+            // console.log("割引価格で購入");
+        }
+    }else if(wallet >= sellPrice){
+        wallet -= sellPrice;
+        // console.log("割引価格で購入");
+    }else{
+        console.log("残高不足です");
+        // console.log(`${i}回購入できました、残り金額は${wallet}円です。`);
+        break;
+    }
 
-// for(let i = 0; wallet < price; i++){
-//     if (wallet >= price) {
-//         wallet = wallet - price
-//         console.log(`${i+1}回目の購入`);
-//         console.log(`残り金額${wallet}円`);
-//     }
-//     console.log(`${i + 1}回購入することができました`);
-//     console.log(`残りの金額は${wallet}円です`);
-// };
+    console.log(`${i + 1}回目の買い物終了、残金は${wallet}円です`);
+
+}
+    
+    // for(let i = 0; wallet < price; i++){
+    //     if (wallet >= price) {
+    //         wallet = wallet - price
+    //         console.log(`${i+1}回目の購入`);
+    //         console.log(`残り金額${wallet}円`);
+    //     }
+    //     console.log(`${i + 1}回購入することができました`);
+    //     console.log(`残りの金額は${wallet}円です`);
+    // };
 
 // wallet = wallet - price;
 // wallet -= price;
